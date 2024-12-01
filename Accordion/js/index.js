370cbe054cb4221baa242a936d0d96e38c9a3e07
Serega <aleksei87-2010@mail.ru>
2024-12-02 01:10:30 +0300

    fix styles

--- Accordion/js/index.js
+++ Accordion/js/index.js
@@ -7,16 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   tabs.forEach((tab, index) => {
     const button = tab.querySelector(".tab");
-    const content = tab.querySelector(".tab-content");
+    const content = tab.querySelector(".accordion-content");
 
     button.addEventListener("click", () => {
       if (tabsOpened[index]) {
-
         content.classList.remove("active");
         tab.classList.remove("active");
         tabsOpened[index] = false;
       } else {
-
         content.classList.add("active");
         tab.classList.add("active");
         tabsOpened[index] = true;
@@ -28,19 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   function checkButtonState() {
-    const allTabsVisited = tabsVisited.every(tab => tab);
-    const allTabsOpened = tabsOpened.every(tab => tab);
-    const allTabsClosed = tabsOpened.every(tab => !tab);
+    const allTabsVisited = tabsVisited.every((tab) => tab);
+    const allTabsOpened = tabsOpened.every((tab) => tab);
+    const allTabsClosed = tabsOpened.every((tab) => !tab);
 
     if (allTabsOpened && allTabsVisited) {
       continueBtn.disabled = false;
       continueBtn.classList.remove("disabled");
-
     } else if (allTabsClosed) {
-
       continueBtn.disabled = true;
       continueBtn.classList.add("disabled");
-
     }
   }
 });
